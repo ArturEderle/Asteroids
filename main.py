d206e5dfc4428b7a746273574a09302f016d6008
Artur Ederle <artur.ederle@web.de>
2025-04-03 10:17:24 +0200

    Player(Triangle) can rotate now

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        pygame.event.pump()
         screen.fill((0, 0, 0))
         dt = clock.tick(60) / 1000
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
