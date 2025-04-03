f38a71706d792f16ac0de09d88a5bb6e6310ff7e
Artur Ederle <artur.ederle@web.de>
2025-04-03 10:57:46 +0200

    Added Shooting Mechanism

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
+
 
 class Player(CircleShape):
 
@@ -23,6 +25,10 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -33,6 +39,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * (-1))
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
